686880aa33a37104a6b54a71d3f7486ad3665528
Lautaro Jofré <lautaro.jofre2004@gmail.com>
2023-05-08 16:18:43 -0300

    cambios

--- src/main/java/com/portfolio/jofre/Controller/CHys.java
+++ src/main/java/com/portfolio/jofre/Controller/CHys.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins="https://frontend-jofrelautaro.web.app",exposedHeaders = {"Access-Control-Allow-Origin","Access-Control-Allow-Credentials"})
+@CrossOrigin(origins="https://frontend-jofrelautaro.web.app",exposedHeaders = {"Access-Control-Allow-Origin"})
 @RequestMapping("/skill")
 public class CHys {
 
