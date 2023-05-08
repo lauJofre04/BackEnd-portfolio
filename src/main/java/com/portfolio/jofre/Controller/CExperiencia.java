fd558b7089e32ce18d79bfa09fdb9031a4c7e6fb
Lautaro Jofré <lautaro.jofre2004@gmail.com>
2023-05-08 15:40:29 -0300

    changes

--- src/main/java/com/portfolio/jofre/Controller/CExperiencia.java
+++ src/main/java/com/portfolio/jofre/Controller/CExperiencia.java
@@ -26,7 +26,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/experiencia")
-@CrossOrigin(origins="https://frontend-jofrelautaro.web.app")//direccion del front de angular
+@CrossOrigin(origins="https://frontend-jofrelautaro.web.app",exposedHeaders = {"Access-Control-Allow-Origin"})//direccion del front de angular
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
