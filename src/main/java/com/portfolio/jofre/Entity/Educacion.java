a6d2f653e767738b797709ca65173518910262e4
Lautaro Jofré <lautaro.jofre2004@gmail.com>
2023-05-12 23:37:18 -0300

    dsfs

--- src/main/java/com/portfolio/jofre/Entity/Educacion.java
+++ src/main/java/com/portfolio/jofre/Entity/Educacion.java
@@ -12,7 +12,6 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.validation.constraints.NotNull;
 import java.util.Date;
